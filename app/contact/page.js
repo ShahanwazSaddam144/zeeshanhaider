e1786ff77df18a92f5e89a47a488ba945ca0ed51
='coder101_js <='op422010@gmail.com>
2025-09-11 23:35:23 +0500

    message left

---
+++ app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center font-bold text-9xl'>
+      Ma school ma ho ga kal , to bus ye bana de 👍
+    </div>
+  )
+}
+
+export default page
